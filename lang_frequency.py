a8a7be7801f9f95c8fd7dc9bccaadc5825dda951
semenchenkovital <semenchenkovital@gmail.com>
2016-10-25 00:36:29 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import codecs
+from collections import Counter
 
 def load_data(filepath):
-    pass
-
-
+    infile=codecs.open(filepath,'r', 'utf-8-sig').read()
+    return infile
 def get_most_frequent_words(text):
-    pass
-
+    data = text.lower()
+    spisok = ['1','2','3','4','5','6','7','8','9','0',
+              '*','-','.',',','\\','/','#','_','&','}','{','[',']','?','!','"',':',';','@','&']
+    for zamena in spisok:
+        data = data.replace(zamena,' ')    
+    data = data.split()
+    data = Counter(data)
+    print (data.most_common(10))
+    
 
 if __name__ == '__main__':
-    pass
+    file = input("Введите имя файла:")
+    try:
+        data = load_data(file)
+    except:
+        print('Некорректное имя файла или путь к файлу')
+    else:
+        get_most_frequent_words(data)
